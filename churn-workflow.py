c47aea05e558643e93851a540c6b896715070d4b
thepranj <singh.pra@husky.neu.edu>
2021-06-16 10:21:57 -0400

    Add files via upload

--- churn-workflow.py
+++ churn-workflow.py
@@ -6,13 +6,11 @@ from verticapy.learn.linear_model import LogisticRegression
 from verticapy.learn.model_selection import cross_validate
 
 def connect():
-    conn_info = {'host': '172.16.61.170',
-                 'port': '5433',
-                 'user': 'dbadmin',
-                 'password': 'password', 
-                 'database': 'test',
-                 'session_label': 'some_label',
-                 'unicode_error': 'strict'}
+    conn_info = {"host": "34.237.154.116", 
+                     "port": "5433", 
+                     "database": "testdrive", 
+                     "password": "password", 
+                     "user": "dbadmin"}
     new_auto_connection(conn_info, name = 'my_cluster')
     change_auto_connection('my_cluster')
     #print(read_auto_connect())           
@@ -30,37 +28,36 @@ def main():
     #print("""Saving to database as 'demo.churn_clean'\n""")
     # Model Train
     print("Begin training")
-    train(churn)
+    churn = train(churn)
     # Predict
     print("Begin predicting")
-    print("""Predictions saved to 'Desktop/pred.csv'\n""")
+    results(churn)
+    #print("""Predictions saved to 'Desktop/churn_results.csv'\n""")
     print('Workflow complete... Exiting')
 
-
-
-
-def main2():
-    connect()
-    churn = vDataFrame('public.churn')
-    val1 = ''
-    intro = '''Please Enter a command: \n1) data prep\n2) train\n3) predict\n4) quit\n\n'''
-    while val1 != '4':
-        val1 = input(intro)
-        if val1 == '1':
-            print("Begin data prep")
-            print("Running one hot encoding on new data...")
-            churn = prep(churn)
-            print("Success!")
-            print("""Saving to database as 'demo.churn_clean'\n""")
-        elif val1 == '2':
-            pass
-        elif val1 == '3':
-            print("Begin predicting")
-            print("""Predictions saved to 'Desktop/pred.csv'\n""")
-            #model = LogReg  
-            #predict(model)  
-        elif val1 == '4':
-            print("quitting\n")
+# Run Main 2 if you want to walk through the workflow step by step
+# def main2():
+#     connect()
+#     churn = vDataFrame('public.churn')
+#     val1 = ''
+#     intro = '''Please Enter a command: \n1) data prep\n2) train\n3) predict\n4) quit\n\n'''
+#     while val1 != '4':
+#         val1 = input(intro)
+#         if val1 == '1':
+#             print("Begin data prep")
+#             print("Running one hot encoding on new data...")
+#             churn = prep(churn)
+#             print("Success!")
+#             print("""Saving to database as 'demo.churn_clean'\n""")
+#         elif val1 == '2':
+#             pass
+#         elif val1 == '3':
+#             print("Begin predicting")
+#             print("""Predictions saved to 'Desktop/pred.csv'\n""")
+#             #model = LogReg  
+#             #predict(model)  
+#         elif val1 == '4':
+#             print("quitting\n")
     
 def prep(churn):
     for column in ["DeviceProtection", 
@@ -76,8 +73,7 @@ def prep(churn):
                 "PhoneService",
                 "StreamingMovies"]:
         churn[column].decode("Yes",1,0)
-    churn.get_dummies().drop(["customerID", 
-                              "gender", 
+    churn.get_dummies().drop(["gender", 
                               "Contract", 
                               "PaymentMethod", 
                               "InternetService"])
@@ -85,7 +81,7 @@ def prep(churn):
     return(churn)                              
 
 def train(churn):
-    drop_model(name = "public.churn_model")
+    drop(name = "public.churn_model")
     model = LogisticRegression("churn_model", 
                            penalty = 'L2', 
                            tol = 1e-6, 
@@ -95,23 +91,28 @@ def train(churn):
     # cross_validate(model, churn, churn.get_columns(exclude_columns = ["churn"]), 'churn')
     print("Fitting logistic regression model...")
     model.fit(churn, 
-          churn.get_columns(exclude_columns = ["churn"]), 
+          churn.get_columns(exclude_columns = ["churn", "customerID"]), 
           'churn')      
     print("Success! demo.churn_model created")
     print("Model AUC: " + str(model.score(method="auc")) + '\n')
     # Begin Predict
     model.predict(churn,
-                  X = churn.get_columns(exclude_columns = ["churn"]),
+                  X = churn.get_columns(exclude_columns = ["churn", "customerID"]),
                   name = 'pred_probs')
     churn.sort({"pred_probs":"desc"})       
-    churn['pred_probs'].dropna()       
+    churn['pred_probs'].dropna()
+    return(churn)
+
+def results(churn):
+    # Saves results with predictions to DB as a new table
+    churn.to_db(name = '"public"."churn_results"', relation_type = 'table')
+    print("New table created in database: churn_results")
+    # Save csv with predictions
+    #churn.to_csv(name = 'churn_results', path = "C:/Users/PSingh5/Desktop/python/")       
 
 def predict(model):
     # Moved to train function
-    # accidentally dropped ID column, add it back
     pass
     
-
-
 if __name__ == "__main__":
     main()
